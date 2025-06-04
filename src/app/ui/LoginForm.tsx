606b9397fece985b04b71a64183d8ce72685eef5
tabetant <antoine.tabet@mail.utoronto.ca>
2025-06-04 11:35:49 +0300

    fixed signupform + implemented login form no ver

--- src/app/ui/LoginForm.tsx
+++ src/app/ui/LoginForm.tsx
@@ -5,11 +5,11 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { supabase } from '@/db/client'
 import Link from 'next/link'
 
-export default function SignupForm() {
+export default function LoginForm() {
     const inputsSchema = z.object({
-        email: z.string().email('Invalid Email Addres'),
-        password: z.string().min(8, 'Password must be at least 8 characters'),
-    });
+        email: z.string().email('Invalid Email Address'),
+        password: z.string().min(8, 'Password are at least 8 characters'),
+    })
 
     type Inputs = z.infer<typeof inputsSchema>;
 
@@ -17,7 +17,7 @@ export default function SignupForm() {
         register,
         handleSubmit,
         watch,
-        formState: { errors }
+        formState: { errors },
     } = useForm<Inputs>({
         resolver: zodResolver(inputsSchema),
         defaultValues: {
@@ -28,23 +28,23 @@ export default function SignupForm() {
 
     const onSubmit: SubmitHandler<Inputs> = async (data) => {
         const { email, password } = data;
-        const { error } = await supabase().auth.signUp({
+        const { error } = await supabase().auth.signInWithPassword({
             email,
             password
         });
         if (error) {
             // error message
         }
-        // success message
+        // success action + message
     }
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input type='email' placeholder='Enter your email address' />
+            <input type='email' placeholder='Enter email address' />
             <p>{errors.email?.message}</p>
-            <input type='password' placeholder='Enter your password' />
+            <input type='password' placeholder='Enter password' />
             <p>{errors.password?.message}</p>
-            <Link className='hover:text-purple:500' href='/login'>Already a member? Log In!</Link>
+            <Link className='hover:text-purple-500' href='/signup'>Not a member? Sign Up!</Link>
         </form>
     )
 }
